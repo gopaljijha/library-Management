e6825ee270f858c7f151e16a6379a10de0781c7b
Gopal Jha <68194511+gopaljijha@users.noreply.github.com>
2023-08-31 20:36:57 +0530

    Update settings.py

--- librarymanagement/librarymanagement/settings.py
+++ librarymanagement/librarymanagement/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'my7mailacc@gmail.com'
-EMAIL_HOST_PASSWORD = 'nofakeaccount@7'
+EMAIL_HOST_USER = 'email@email.com'
+EMAIL_HOST_PASSWORD = 'emailPassword'
